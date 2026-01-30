a387a49fb9f35cc52e7c620c81316757682ae162
SherazAhmad12345 <119704391+SherazAhmad12345@users.noreply.github.com>
2026-01-30 16:13:59 -0500

    Add comments to MainActivity for clarity
    Added comments to clarify the functionality of the login and register methods.

--- app/src/main/java/com/example/eecs4443lab/MainActivity.java
+++ app/src/main/java/com/example/eecs4443lab/MainActivity.java
@@ -11,7 +11,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
 	Validation validation = new Validation();
-
+    
+	//Default code for view empty activity android studio template
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,19 +26,22 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	}
-
-	public void login(View view) {
+    //Takes users login information and verifies if it matches the accounts from the credentials.txt file.
+	public void login(View view)
+		//Gets the username and password id's from the XML file.
 		String username = findViewById(R.id.username).getText().toString();
 		String password = findViewById(R.id.password).toString();
-
+        //verifies credentials and checks to see if it matches the names fromt the credentials.txt file.
 		if (validation.validateCredentials(username, password)) {
-			// SUCCESS
+			// if it matches, it shows the user the welcome page.
 		} else {
-			// FAILURE
+			// if it fails, it will say that the users credentials do not match and won't change pages.
 		}
 	}
 
+    //Allows a new user to be added to the sytem.
 	public void register(String username, String password) {
 		validation.addCredentials(username, password);
 	}
 }
+
