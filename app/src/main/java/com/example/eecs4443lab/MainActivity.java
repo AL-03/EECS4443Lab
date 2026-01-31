40144fcd16b402b3857f0133ce05d475288141f3
Alice L <aliceyyl2003@gmail.com>
2026-01-31 10:22:01 -0500

    Created Welcome activity and connected it to Main activity

--- app/src/main/java/com/example/eecs4443lab/MainActivity.java
+++ app/src/main/java/com/example/eecs4443lab/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.eecs4443lab;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Login button var
+    private Button loginButton;
+    String username= "Jessie";  // Change this to what the actual username is
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,18 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Connect to login button in xml
+        loginButton = findViewById(R.id.login);
+        // Change screen to Welcome screen when login button pressed
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                intent.putExtra("username", username);
+                startActivity(intent);
+            }
         });
     }
 }
