7d592d8f4fe42b12c0c48c6ced5aeead3d412e0f
SherazAhmad12345 <119704391+SherazAhmad12345@users.noreply.github.com>
2026-02-01 23:27:01 -0500

    Add files via upload

--- app/src/main/java/com/example/eecs4443lab/MainActivity.java
+++ app/src/main/java/com/example/eecs4443lab/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.eecs4443lab;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.activity.EdgeToEdge;
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
   private TextInputEditText username;
   private TextInputEditText password;
+  private SharedPreferences sharedPreferences;
+  private CheckBox rememberMeCheckbox;
 	Validation validation = new Validation();
 	TextView messageBox = findViewById(R.id.messageBox);
     
@@ -36,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     
     // Connect to cancel button in xml
     Button cancelButton = findViewById(R.id.cancelButton);
+    rememberMeCheckbox = findViewById(R.id.rememberMe);
     // When user presses cancel, clear the inputted text
     cancelButton.setOnClickListener(new View.OnClickListener() {
         @Override
@@ -54,31 +61,29 @@ public class MainActivity extends AppCompatActivity {
 	public void login(View view)
 	{
 		//Gets the username and password id's from the XML file.
-		String username = findViewById(R.id.username).getText().toString();
-		String password = findViewById(R.id.password).toString();
+		String usernameStr = username.getText().toString();
+		String passwordStr = password.toString();
         //verifies credentials and checks to see if it matches the names fromt the credentials.txt file.
-		if (validation.validateCredentials(username, password)) {
+		if (validation.validateCredentials(usernameStr, passwordStr)) {
 			// if it matches, it shows the user the welcome page. Additionally, if the remember me checkbox is checked, it will save users credentials;
 	       if (rememberMeCheckbox.isChecked()){
-            SharedPreferences.Editor editor=sharedPreferences.edit();
+            SharedPreferences.Editor editor= sharedPreferences.edit();
             editor.putBoolean("loggedIn", true);
-            editor.putString("username", usernameInput);
+            editor.putString("username", usernameStr);
             editor.apply();
 		   }
 		    messageBox.setText("");
-	        Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);	
-	        intent.putExtra("username", username);
-         	startActivity(intent);
+	        // Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+	        //  intent.putExtra("username", usernameStr);
+         	//startActivity(intent);
 		
 		} else {
 			// if it fails, it will say that the users credentials do not match and won't change pages.
             messageBox.setText("Login failed, please try again.");
 		}
 	 }
-	}
 
-    //Allows a new user to be added to the sytem.
-	public void register(String username, String password) {
-		validation.addCredentials(username, password);
-	}
+
+    //Allows a new user to be added to the sytsem.
+
 }
