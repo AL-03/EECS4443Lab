3db6e3e2814e180aca2e1916cd372f20852dbeb8
Alice L <aliceyyl2003@gmail.com>
2026-02-01 22:17:35 -0500

    Added cancel button logic

--- app/src/main/java/com/example/eecs4443lab/MainActivity.java
+++ app/src/main/java/com/example/eecs4443lab/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.eecs4443lab;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class MainActivity extends AppCompatActivity {
+    private TextInputEditText username;
+    private TextInputEditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,21 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Connect to cancel button in xml
+        Button cancelButton = findViewById(R.id.cancelButton);
+        // When user presses cancel, clear the inputted text
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Access username and password TextInputEditText fields from xml
+                username = findViewById(R.id.username);
+                password = findViewById(R.id.password);
+                // Set both username and password to empty
+                username.setText("");
+                password.setText("");
+            }
         });
     }
 }
